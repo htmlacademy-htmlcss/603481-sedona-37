97a1a703dd9f9672c378096e4f1dd5c9e7120b2b
ekburalec <drmax782@gmail.com>
2023-10-10 12:43:57 +0500

    Поправил проект

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,6 @@
 // Dialog 
 const modalOpen = document.querySelector(".modal-open")
-const modalClose = document.querySelector(".modal-close-button")
+const modalClose = document.querySelector(".modal-button-close")
 const dialog = document.querySelector(".modal-window")
 
 modalOpen.addEventListener("click", function() {
